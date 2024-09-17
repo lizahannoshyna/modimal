9a8d1a239b9a8ddff7f67c2507214813f8e9dcef
Liza Hannoshyna <fox8pro8@gmail.com>
2024-09-17 20:28:05 +0300

    swiper

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ document.querySelectorAll('.heart').forEach(function(heart) {
         heart.classList.toggle('filled'); // Добавляем или убираем класс 'filled' при клике
     });
 });
+
+
+
+
+import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs";
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 4,
+  spaceBetween: 30,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  });
