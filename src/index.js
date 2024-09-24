dd144e6a394bc6a30996b5c5925fa513e55c1a09
lega2010 <lega10@i.ua>
2024-09-24 20:09:12 +0300

    faqsd

--- src/index.js
+++ src/index.js
@@ -20,3 +20,50 @@ const swiper = new Swiper('.mySwiper', {
   },
   
   });
+
+  //FAQS
+
+  document.querySelectorAll('.faq-item__header').forEach(header => {
+    header.addEventListener('click', function () {
+        const content = this.nextElementSibling;
+        const toggle = this.querySelector('.faq-item__toggle');
+
+        // Используем класс для анимации
+        content.classList.toggle('faq-item__content--open');
+
+        // Переключаем знак с анимацией вращения
+        if (toggle.textContent === '+') {
+            toggle.textContent = '−';
+        } else {
+            toggle.textContent = '+';
+        }
+
+        toggle.classList.toggle('rotate'); // добавляем класс для вращения
+    });
+});
+
+let isGreen = false; // Переменная для отслеживания состояния цвета
+
+document.getElementById('changeColor').addEventListener('click', function () {
+    const textElement = document.getElementById('active_green');
+    if (isGreen) {
+        textElement.style.color = ''; // Возвращаем исходный цвет
+    } else {
+        textElement.style.color = '#748C70'; // Меняем цвет на зеленый
+        textElement.style.transition = 'color 0.2s ease';
+    }
+    isGreen = !isGreen; // Переключаем состояние
+});
+
+let isGreen2 = false; // Переменная для отслеживания состояния цвета
+
+document.getElementById('changeColor2').addEventListener('click', function () {
+    const textElement = document.getElementById('active_green2');
+    if (isGreen2) {
+        textElement.style.color = ''; // Возвращаем исходный цвет
+    } else {
+        textElement.style.color = '#748C70'; // Меняем цвет на зеленый
+        textElement.style.transition = 'color 0.2s ease';
+    }
+    isGreen2 = !isGreen2; // Переключаем состояние
+});
