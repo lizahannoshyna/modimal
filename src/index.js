c9bb255c916248f0ae3f5ab3c16f9c552bd73428
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2024-10-05 20:27:09 +0300

    initrial commit

--- src/index.js
+++ src/index.js
@@ -7,19 +7,45 @@ document.querySelectorAll('.heart').forEach(function (heart) {
 
 
 
-import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs";
-const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // Для екранів менше 991px
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true, // додатково, якщо бажаєте динамічні кулі
+        },
+        navigation: false, // Вимкнути кнопки навігації
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      // Для екранів 991px і більше
+      991: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+        pagination: false, // Вимкнути пагінацію
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    },
+  });
 
 //FAQS
 
