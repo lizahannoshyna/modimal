eb10664813a90e84042b212594e07cf8bb9c6e90
DevLega <97284650+DevLega@users.noreply.github.com>
2024-10-05 18:35:13 +0000

    Deploying to gh-pages from @ lizahannoshyna/modimal@d9bccad304820006e68c8e24f445c8a5027e9365 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/index-7f41c136.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
+import"./assets/index-2c546dd8.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
 //# sourceMappingURL=commonHelpers3.js.map
