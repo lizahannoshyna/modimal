ef3c4fa2ddd9a6c37a80c615c20e4252faeb707e
Liza Hannoshyna <fox8pro8@gmail.com>
2024-10-05 19:54:23 +0300

    burger

---
+++ src/header.js
@@ -0,0 +1,10 @@
+const navInput = document.querySelector('.nav-input');
+const body = document.querySelector('body');
+
+navInput.addEventListener('change', () => {
+    if (navInput.checked) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+    }
+});
