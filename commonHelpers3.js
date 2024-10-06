ad711a1ccfa7cb2263a89a910e2241c7d2b9b76a
lizahannoshyna <91091190+lizahannoshyna@users.noreply.github.com>
2024-10-06 12:18:24 +0000

    Deploying to gh-pages from @ lizahannoshyna/modimal@c15e56d961570b5ba85376e6487068a32b606865 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/index-c01993df.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
+import"./assets/index-86874b07.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
 //# sourceMappingURL=commonHelpers3.js.map
