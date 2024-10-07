1e04895251b6f409064467fde18586ea44af7763
VladpressF <149525338+VladpressF@users.noreply.github.com>
2024-10-07 18:14:34 +0000

    Deploying to gh-pages from @ lizahannoshyna/modimal@1646839badb023a51cee58c77f6ca70e5e1c9faf 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/index-344e6049.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
+import"./assets/index-88c843e8.js";const e=document.querySelector(".nav-input"),o=document.querySelector("body");e.addEventListener("change",()=>{e.checked?o.classList.add("no-scroll"):o.classList.remove("no-scroll")});
 //# sourceMappingURL=commonHelpers3.js.map
